6090d6be0a5c741b9bef2200cdf680e8c7b38fd9
aseelflihan <aseelflihan@gmail.com>
2023-03-20 01:04:26 +0300

    complate assignment

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,10 +1,16 @@
 import React from "react";
 
-function Filter({ onCategoryChange }) {
+function Filter({ search, onSearchChange, onCategoryChange }) {
+  const handleSearchChange = (e) => {
+    onSearchChange(e.target.value)
+  }
+  const handleCategoryChange = (e) => {
+    onCategoryChange(e.target.value)
+  }
   return (
     <div className="Filter">
-      <input type="text" name="search" placeholder="Search..." />
-      <select name="filter" onChange={onCategoryChange}>
+      <input type="text" name="search" placeholder="Search..." value={search} onChange={handleSearchChange} />
+      <select name="filter" onChange={handleCategoryChange}>
         <option value="All">Filter by category</option>
         <option value="Produce">Produce</option>
         <option value="Dairy">Dairy</option>
--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,17 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { v4 as uuid } from "uuid";
 
 function ItemForm(props) {
+  const { onItemFormSubmit } = props;
+  const [ name, setName ] = useState("");
+  const [ category, setCategory ] = useState("Produce");
+
+  const handleNameChange = (e) => {
+    setName(e.target.value)
+  }
+
+  const handleCategoryChange = (e) => {
+    setCategory(e.target.value)
+  }
+
+  const addElement = (e) => {
+    e.preventDefault();
+    onItemFormSubmit({
+      id: uuid(),
+      name,
+      category,
+    });
+  }
   return (
-    <form className="NewItem">
+    <form className="NewItem" onSubmit={addElement}>
       <label>
         Name:
-        <input type="text" name="name" />
+        <input type="text" name="name" value={name} onChange={handleNameChange} />
       </label>
 
       <label>
         Category:
-        <select name="category">
+        <select name="category" value={category} onChange={handleCategoryChange}>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
